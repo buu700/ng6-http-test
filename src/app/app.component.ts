2b706b4b0ee6b31fd6394e07e38cc70d98fd5700
Ryan Lester <hacker@linux.com>
2018-05-02 19:27:27 -0400

    balls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit () {
+    this.httpClient.request(
+      new HttpRequest('GET', 'https://na.cdn.cyph.com/cyph.ws/current')
+    ).subscribe(
+      event => {
+        console.log({event});
+      }
+    );
+  }
+
+  constructor (private readonly httpClient: HttpClient) {}
 }
